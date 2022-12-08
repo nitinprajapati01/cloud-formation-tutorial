b3b44b189b3e106799faa2455c3b1f601e14c18d
NITIN Prajapati <91796080+nitinprajapati01@users.noreply.github.com>
2022-12-08 12:14:09 +0530

    Initial Commit

---
+++ aws-glue/changes/join_marketing_and_sales_data.py
@@ -0,0 +1,37 @@
+import sys
+import pyspark.sql.functions as func
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 's3_output_path', 's3_sales_data_path', 's3_marketing_data_path'])
+s3_output_path = args['s3_output_path']
+s3_sales_data_path = args['s3_sales_data_path']
+s3_marketing_data_path = args['s3_marketing_data_path']
+
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+salesForecastedByDate_DF = \
+    glueContext.spark_session.read.option("header", "true")\
+        .load(s3_sales_data_path, format="parquet")
+
+mktg_DF = \
+    glueContext.spark_session.read.option("header", "true")\
+        .load(s3_marketing_data_path, format="parquet")
+
+
+salesForecastedByDate_DF\
+    .join(mktg_DF, 'date', 'inner')\
+    .orderBy(salesForecastedByDate_DF['date']) \
+    .write \
+    .format('csv') \
+    .option('header', 'true') \
+    .mode('overwrite') \
+    .save(s3_output_path)
---
+++ aws-glue/changes/process_marketing_data.py
@@ -0,0 +1,43 @@
+import sys
+import pyspark.sql.functions as func
+from awsglue.dynamicframe import DynamicFrame
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 's3_output_path', 'database_name', 'table_name'])
+s3_output_path = args['s3_output_path']
+database_name = args['database_name']
+table_name = args['table_name']
+
+sc = SparkContext.getOrCreate()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+mktg_DyF = glueContext.create_dynamic_frame\
+    .from_catalog(database=database_name, table_name=table_name)
+
+mktg_DyF = ApplyMapping.apply(frame=mktg_DyF, mappings=[
+    ('date', 'string', 'date', 'string'),
+    ('new visitors seo', 'bigint', 'new_visitors_seo', 'bigint'),
+    ('new visitors cpc', 'bigint', 'new_visitors_cpc', 'bigint'),
+    ('new visitors social media', 'bigint', 'new_visitors_social_media', 'bigint'),
+    ('return visitors', 'bigint', 'return_visitors', 'bigint'),
+        ], transformation_ctx='applymapping1')
+
+mktg_DyF.printSchema()
+
+mktg_DF = mktg_DyF.toDF()
+
+mktg_DF.write\
+    .format('parquet')\
+    .option('header', 'true')\
+    .mode('overwrite')\
+    .save(s3_output_path)
+
+job.commit()
